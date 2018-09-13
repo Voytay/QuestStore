81e9494b17c0640991ef6791e4697e24d0ad0503
Paul <bielcu@gmail.com>
2018-09-13 20:07:55 +0200

    Add getRoleBySession method to LoginDAO class

--- src/main/java/com/codecool/Queststore/DAO/LoginDAO.java
+++ src/main/java/com/codecool/Queststore/DAO/LoginDAO.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 
 
 public class LoginDAO extends Connectable {
-    public String verification(String id){
+    public String verification(String id) throws SQLException {
         String userID;
         Connection con = getConnection();
         try {
@@ -23,6 +23,34 @@ public class LoginDAO extends Connectable {
             e.printStackTrace();
             throw new RuntimeException();
         }
+        con.close();
         return null;
+    }
+    public int getRoleBySession(String session_id) throws SQLException {
+        String userID;
+        int personRole;
+        Connection con = getConnection();
+        try {
+            Statement statement = con.createStatement();
+            statement.execute("SELECT user_id FROM session WHERE " +
+                    "session_id = " + session_id + ";");
+            ResultSet resultSet = statement.getResultSet();
+            if (resultSet.next()) {
+                userID = resultSet.getString("user_id");
+                statement.execute("SELECT role FROM person WHERE " +
+                        "user_id = " + userID + ";");
+                        resultSet = statement.getResultSet();
+                if (resultSet.next()) {
+                    personRole = resultSet.getInt("session_id");
+                    return personRole;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+        con.close();
+        System.out.println("No such session or person");
+        return 0;
     }
 }
