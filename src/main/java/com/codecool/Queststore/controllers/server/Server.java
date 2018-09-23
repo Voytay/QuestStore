8d263d127fe040bed5b7b18ee0efed16826ff28c
SStreich <selene.streich@gmail.com>
2018-09-23 16:28:54 +0200

    Add draft version of creatementorhandler for tests

--- src/main/java/com/codecool/Queststore/controllers/server/Server.java
+++ src/main/java/com/codecool/Queststore/controllers/server/Server.java
@@ -1,6 +1,7 @@
 package com.codecool.Queststore.controllers.server;
 
 import com.codecool.Queststore.controllers.server.handlers.AdminHandler;
+import com.codecool.Queststore.controllers.server.handlers.CreateMenHandler;
 import com.codecool.Queststore.controllers.server.handlers.LoginPageHandler;
 import com.codecool.Queststore.controllers.server.handlers.Static;
 import com.sun.net.httpserver.HttpServer;
@@ -15,6 +16,10 @@ public class Server {
             server.createContext("/log", new LoginPageHandler());
             server.createContext("/static", new Static());
             server.createContext("/admin", new AdminHandler());
+            server.createContext("/create_mentor", new CreateMenHandler());
+            server.createContext("/edit_mentor", new AdminHandler());
+            server.createContext("/show_mentors", new AdminHandler());
+
             server.setExecutor(null);
             server.start();
         } catch (
