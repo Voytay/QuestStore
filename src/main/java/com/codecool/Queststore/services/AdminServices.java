8913e6d76247765140dc98dc97ad762e316f9829
SStreich <selene.streich@gmail.com>
2018-09-23 22:22:34 +0200

    Fix services

--- src/main/java/com/codecool/Queststore/services/AdminServices.java
+++ src/main/java/com/codecool/Queststore/services/AdminServices.java
@@ -1,27 +1,19 @@
 package com.codecool.Queststore.services;
 
 import com.codecool.Queststore.DAO.MentorDAO;
+import com.codecool.Queststore.DAO.SessionDAO;
 import com.codecool.Queststore.models.User;
-import org.apache.commons.lang3.RandomStringUtils;
+import com.codecool.Queststore.queries.AllMentors;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
-import java.math.BigInteger;
-import java.security.MessageDigest;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 
-public class AdminServices {
+public class AdminServices extends Services {
     private final int ADMIN = 1;
+    private final int MENTOR = 2;
 
-//    public String getCreateMentPage(User user) {
-//        if(user.getROLE() == ADMIN) {
-//            JtwigTemplate template = JtwigTemplate.classpathTemplate("static/templates/create_mentor.twig.html");
-//            JtwigModel model = JtwigModel.newModel().with("name", user.getNAME());
-//            String response = template.render(model);
-//            return response;
-//        }
-//        return null;
-//    }
 
     public String getCreateMenPage() {
             JtwigTemplate template = JtwigTemplate.classpathTemplate("static/templates/create_mentor.twig.html");
@@ -30,39 +22,20 @@ public class AdminServices {
             return response;
     }
 
-    public String hashPassword(String password, String salt) {
-        String toHash = password + salt;
-        try {
-            MessageDigest m = MessageDigest.getInstance("MD5");
-            m.update(toHash.getBytes(), 0, toHash.length());
-            String  hashed = new BigInteger(1, m.digest()).toString(16);
-            System.out.println(hashed);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return toHash;
-    }
-
-    public String getRandomSalt() {
-        return RandomStringUtils.randomAlphanumeric(5);
-    }
-
     public void createMentor(Map<String, String> data) throws SQLException {
-        int role = 2;
-        String salt = getRandomSalt();
-        String passwdHash = hashPassword(data.get("password"), salt);
-        User user = new User(data.get("username"), passwdHash, salt, data.get("first_name"),
-                data.get("last_name"), role, data.get("email"), data.get("phone_number"));
+        User user = createUser(data, MENTOR);
         MentorDAO mentorDAO = new MentorDAO();
         mentorDAO.insertRecord(user);
     }
 
-    public String getShowMenPage() {
-//            MentorDAO menDao = new MentorDAO();
-//            List<User> mentors = menDao.getMentors();
-            JtwigTemplate template = JtwigTemplate.classpathTemplate("static/templates/show_mentors.twig.html");
-            JtwigModel model = JtwigModel.newModel().with("name", "Beniz");
-            //model.with("mentors", mentors);
+    public String getShowMenPage(String sessionID) throws SQLException {
+        SessionDAO sessionDAO = new SessionDAO();
+        String username = sessionDAO.getUserNamebySession(sessionID);
+        MentorDAO menDao = new MentorDAO();
+        List<User> mentors = menDao.getRecordsList(new AllMentors());
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("static/templates/show_mentors.twig.html");
+        JtwigModel model = JtwigModel.newModel().with("username", username);
+           model.with("mentors", mentors);
             String response = template.render(model);
             return response;
     }
--- src/main/java/com/codecool/Queststore/services/Services.java
+++ src/main/java/com/codecool/Queststore/services/Services.java
@@ -1,4 +1,42 @@
 package com.codecool.Queststore.services;
 
-public class Services {
+import com.codecool.Queststore.DAO.PersonDAO;
+import com.codecool.Queststore.models.User;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.sql.SQLException;
+import java.util.Map;
+
+public abstract class Services {
+
+    public User createUser(Map<String, String> data, int role) throws SQLException {
+        String salt = getRandomSalt();
+        String passwdHash = hashPassword(data.get("password"), salt);
+        User user = new User(data.get("username"), passwdHash, salt, data.get("fname"),
+                data.get("lname"), role, data.get("email"), data.get("phone"));
+        PersonDAO personDAO = new PersonDAO();
+        personDAO.insertRecord(user);
+        return user;
+    }
+
+    public String hashPassword(String password, String salt) {
+        String toHash = password + salt;
+        String hashed = "";
+        try {
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            m.update(toHash.getBytes(), 0, toHash.length());
+            hashed = new BigInteger(1, m.digest()).toString(16);
+            System.out.println(hashed);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return hashed;
+    }
+
+    public String getRandomSalt() {
+        return RandomStringUtils.randomAlphanumeric(5);
+    }
+
 }
